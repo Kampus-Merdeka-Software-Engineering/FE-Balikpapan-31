f227e514bd6d181b23ade93774255edb7807c46b
faiszaslam7 <faizaslam451@gmail.com>
2023-10-02 20:41:41 +0700

    Fixed some errors

--- script/navbar.js
+++ script/navbar.js
@@ -12,18 +12,3 @@ burgerMenu.addEventListener('click',()=>{
          isOpen=false;
     }
 })
-
-const login= document.querySelector('.navbar .menus1 li:nth-child(5)');
-const consult = document.createElement('a');
-consult.href="Consult.html";
-consult.innerHTML ='Consult';
-login.innerHTML= '';
-login.appendChild(consult);
-console.log(login.innerHTML);
-
-const login2= document.querySelector('.navbar .menus2 ul li:nth-child(5)');
-const consult2 = document.createElement('a');
-consult2.href="Consult.html";
-consult2.innerHTML ='Consult';
-login2.innerHTML= '';
-login2.appendChild(consult2);
